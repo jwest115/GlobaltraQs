1fc794b366a053364cfa2b687a3a93184265825a
redxzeta <xzetanzero@gmail.com>
2019-10-04 09:44:36 -0700

    routing experimenting

--- GlobalTraqs/frontend/src/App.js
+++ GlobalTraqs/frontend/src/App.js
@@ -2,7 +2,8 @@
 import React, { Component, Fragment } from 'react'
 import Header from './components/layout/Header'
 import MapDashboard from './components/Map/MapDashboard'
-
+import About from './components/AboutPage/About'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 const divStyle = {
     height: '400px',
     width: '100%'
@@ -12,12 +13,16 @@ const divStyle = {
 export class App extends Component {
     render() {
         return (
-            <Fragment>
-                <Header />
-                <div className="container" style={divStyle}>
-                    <MapDashboard />
-                </div>
-            </Fragment>
+            <Router>
+                <Fragment>
+                    <Header />
+                    <div className="container" style={divStyle}>
+
+                        <Route path="/" component={MapDashboard} />
+                        <Route path="/about" component={About} />
+                    </div>
+                </Fragment>
+            </Router>
         )
     }
 }
