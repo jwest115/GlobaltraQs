3ac7fe0031bd30d5d66dce2b1eac741e5f3e8e57
redxzeta <xzetanzero@gmail.com>
2019-10-17 10:42:17 -0700

    fixing issues and adding css, style loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,15 @@ module.exports = {
             {
                 test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
                 loader: 'url-loader?limit=500000'
-            }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+
         ]
     }
 };
