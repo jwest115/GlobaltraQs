ae412b846d9cc111a9230e63b62000f4a701da08
redxzeta <xzetanzero@gmail.com>
2019-10-17 00:34:28 -0700

    minor fixing

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
       {
         test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
         loader: 'url-loader?limit=500000'
-      }]
-    }
+      }
+    ]
   }
+}
