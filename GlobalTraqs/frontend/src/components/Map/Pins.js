517f3fc5b754ac1d36b035432c48d76dc6348394
Nate <xzetanzero@gmail.com>
2019-11-20 13:31:25 -0800

    testing - aff

--- GlobalTraqs/frontend/src/components/Map/Pins.js
+++ GlobalTraqs/frontend/src/components/Map/Pins.js
@@ -12,7 +12,7 @@ import { Link } from "react-router-dom";
 import EditPin from "./EditPin";
 import L from "leaflet";
 import Modal from "./Modal";
-
+import Control from "react-leaflet-control";
 import MarkerClusterGroup from "react-leaflet-markercluster";
 const divStyle = {
   height: "90%",
@@ -128,6 +128,14 @@ export class Pins extends Component {
             attribution="Map tiles by <a href='http://stamen.com'>Stamen Design</a>, <a href='http://creativecommons.org/licenses/by/3.0'>CC BY 3.0</a> &mdash; Map data &copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
             url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png"
           />
+          <Control position="topright">
+            <button
+              onClick={() => this.createStory(true)}
+              className="btn btn-primary add-story-button"
+            >
+              Add Story
+            </button>
+          </Control>
           <MarkerClusterGroup>
             {this.props.pins.map((marker, index) => {
               let post = [marker.latitude, marker.longitude];
@@ -188,12 +196,6 @@ export class Pins extends Component {
             owner={userid}
           />
         ) : null}
-        {/*   <button
-          onClick={() => this.createStory(true)}
-          className="btn btn-primary add-story-button"
-        >
-          Add Story
-        </button> */}
 
         {/*<PinForm userlat={this.state.userlat} userlng={this.state.userlng} />*/}
         {/* change AddPin PinForm for working form */}
