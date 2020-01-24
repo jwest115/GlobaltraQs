cdd356c25aa3c47fcc0c1976d3695061eca9045c
Nate <xzetanzero@gmail.com>
2020-01-24 09:11:36 -0800

    Pillow Line didnt get in for some reason

--- GlobalTraqs/GlobalTraqs/settings.py
+++ GlobalTraqs/GlobalTraqs/settings.py
@@ -171,3 +171,6 @@ STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = ['https://localhost:3000']
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+OPTIMIZED_IMAGE_METHOD = 'pillow'
