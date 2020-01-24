45127704d64cc30a9c8021fa44cebf8b246f0f9b
Nate <xzetanzero@gmail.com>
2020-01-24 09:12:43 -0800

    settingss for Pillow imaging

--- GlobalTraqs/GlobalTraqs/urls.py
+++ GlobalTraqs/GlobalTraqs/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('frontend.urls')),
@@ -27,3 +29,4 @@ urlpatterns = [
     path('', include('accounts.urls')),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
