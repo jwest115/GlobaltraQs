e4c552373c1df940e7d1382cb4bad8298386e284
Nate <nsuarez.22@gmail.com>
2020-01-28 22:49:12 -0800

    image upload in profile

--- GlobalTraqs/frontend/src/components/profile/ProfilePage.js
+++ GlobalTraqs/frontend/src/components/profile/ProfilePage.js
@@ -6,11 +6,11 @@ import Typography from "@material-ui/core/Typography";
 import { login } from "../../actions/auth";
 import { getPins } from "../../actions/pins";
 import axios from "axios";
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import { Avatar } from 'antd';
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import { Avatar } from "antd";
 import { getUser } from "../../actions/users";
 import { getPinsByOwner } from "../../actions/pins";
-import { Markup } from 'interweave';
+import { Markup } from "interweave";
 
 export class ProfilePage extends Component {
   static propTypes = {
@@ -25,21 +25,21 @@ export class ProfilePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      stories: "",
+      stories: ""
     };
   }
 
   updateProfileId = id => {
     this.props.getUser(id);
-    };
+  };
 
   componentDidUpdate(prevProps) {
-  if (this.props.match.params !== prevProps.match.params) {
-        // call the fetch function again
+    if (this.props.match.params !== prevProps.match.params) {
+      // call the fetch function again
       console.log("url changes" + this.props.match.params.id);
-        this.updateProfileId(this.props.match.params.id);
-      }
+      this.updateProfileId(this.props.match.params.id);
     }
+  }
 
   componentDidMount() {
     const { id } = this.props.match.params;
@@ -78,20 +78,17 @@ export class ProfilePage extends Component {
     const { isAuthenticated, login, user } = this.props.auth;
 
     const authLinks = (
-        <Link
-          to={`/users/${id}/settings`}
-          params={{ testvalue: "hello" }}
-        >
-          <button type="button" className="btn btn-primary btn-sm">
-            Settings
-          </button>
-        </Link>
+      <Link to={`/users/${id}/settings`} params={{ testvalue: "hello" }}>
+        <button type="button" className="btn btn-primary btn-sm">
+          Settings
+        </button>
+      </Link>
     );
 
     let isOwner = false;
 
-    if(user != null && user.id == id) {
-        isOwner = true;
+    if (user != null && user.id == id) {
+      isOwner = true;
     }
 
     // const guestLinks = <div><Redirect to="/" /></div>;
@@ -99,41 +96,51 @@ export class ProfilePage extends Component {
 
     return (
       <div>
-          {this.props.userProfile ? (
-         <div>
-         <div>
-          <Typography variant="h5" component="h3" align="center">
-          <Avatar size={64} icon="user" />
-            {this.props.userProfile ? ` ${this.props.userProfile.username}'s Profile Page` : ""}
-              <p>
-                <strong>Bio: </strong>
-                {this.props.userProfile.bio}
-              </p>
-          </Typography>
-             {isOwner ? authLinks : guestLinks}
-        </div>
-        <div class="card">
-           <div class="card-body">
+        {this.props.userProfile ? (
+          <div>
+            <div>
+              <Typography variant="h5" component="h3" align="center">
+                {this.props.userProfile.image_url ? (
+                  <img src={this.props.userProfile.image_url} />
+                ) : (
+                  <Avatar size={64} icon="user" />
+                )}
+                {this.props.userProfile
+                  ? ` ${this.props.userProfile.username}'s Profile Page`
+                  : ""}
+                <p>
+                  <strong>Bio: </strong>
+                  {this.props.userProfile.bio}
+                </p>
+              </Typography>
+              {isOwner ? authLinks : guestLinks}
+            </div>
+            <div class="card">
+              <div class="card-body">
                 {this.props.pins.map((story, index) => {
                   return (
-                      <div style={ {padding: "20px"} }>
-                        <h5 class="card-title" key={index}>{story.title} <br/></h5>
-                        <Markup content={story.description}/>
-                        <Link
-                          to={`/Story/${story.id}`}
-                          params={{ testvalue: "hello" }}
-                          key={index}
+                    <div style={{ padding: "20px" }} key={index}>
+                      <h5 class="card-title">
+                        {story.title} <br />
+                      </h5>
+                      <Markup content={story.description} />
+                      <Link to={`/Story/${story.id}`}>
+                        <button
+                          type="button"
+                          className="btn btn-primary btn-sm"
                         >
-                        <button type="button" className="btn btn-primary btn-sm">
                           View Story
                         </button>
-                        </Link>
-                      </div>
-                  )
+                      </Link>
+                    </div>
+                  );
                 })}
-           </div>
-        </div>
-      </div> ) : ""}
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
@@ -147,4 +154,6 @@ const mapStateToProps = state => ({
 
 ProfilePage.propTypes = {};
 
-export default connect(mapStateToProps, { login, getUser, getPinsByOwner })(ProfilePage);
+export default connect(mapStateToProps, { login, getUser, getPinsByOwner })(
+  ProfilePage
+);
