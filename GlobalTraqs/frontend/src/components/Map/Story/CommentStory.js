4569b95dd7e3957a8d184d679a3ab01616fd9ec8
Nate <nsuarez.22@gmail.com>
2020-02-14 08:13:33 -0800

    changing for delete

--- GlobalTraqs/frontend/src/components/Map/Story/CommentStory.js
+++ GlobalTraqs/frontend/src/components/Map/Story/CommentStory.js
@@ -22,23 +22,33 @@ function CommentStory(props) {
       {props.comment.map((userComment, index) => {
         return (
           <div className="card border-primary mb-3 col-md-6" key={index}>
-            <div className="card-header">{userComment.is_anonymous_comment ? "Anonymous" : userComment.username}</div>
+            <div className="card-header">
+              {userComment.is_anonymous_comment
+                ? "Anonymous"
+                : userComment.username}
+            </div>
             <div className="card-body">
               {/*<h4 className="card-title">{userComment.description}</h4>*/}
               <p className="card-text">
-                  {userComment.description}
+                {userComment.description}
                 {/*Some quick example text to build on the card title and make up*/}
                 {/*the bulk of the card's content.*/}
               </p>
-              <button
-                type="button"
-                className="btn btn-primary btn-sm"
-                onClick={e => {
-                  dispatch(deleteComment(userComment.id));
-                }}
-              >
-                Delete
-              </button>
+              {props.user.id === userComment.commenter ||
+              props.user.is_moderator ||
+              props.user.is_administrator ? (
+                <button
+                  type="button"
+                  className="btn btn-primary btn-sm"
+                  onClick={e => {
+                    dispatch(deleteComment(userComment.id));
+                  }}
+                >
+                  Delete
+                </button>
+              ) : (
+                ""
+              )}
             </div>
           </div>
         );
@@ -50,41 +60,40 @@ function CommentStory(props) {
 export default CommentStory;
 
 const AddCommentForm = props => {
-  if(props.user) {
-      return (
-          <div className="card border-primary mb-3 col-md-6">
-              <div className="card-header">{props.user.username}</div>
-              <div className="card-body">
-                  <h4 className="card-title">Post a Comment</h4>
-                  <form onSubmit={props.onSubmitComment}>
-                      <div className="form-group">
-            <textarea
+  if (props.user) {
+    return (
+      <div className="card border-primary mb-3 col-md-6">
+        <div className="card-header">{props.user.username}</div>
+        <div className="card-body">
+          <h4 className="card-title">Post a Comment</h4>
+          <form onSubmit={props.onSubmitComment}>
+            <div className="form-group">
+              <textarea
                 className="form-control"
                 id="exampleTextarea"
                 rows="3"
                 onChange={e =>
-                    props.setuserComment({
-                        ...props.userComment,
-                        is_anonymous_comment: props.user.is_anonymous_active,
-                        description: e.target.value
-                    })
+                  props.setuserComment({
+                    ...props.userComment,
+                    is_anonymous_comment: props.user.is_anonymous_active,
+                    description: e.target.value
+                  })
                 }
-            ></textarea>
-                      </div>
-                      <button type="submit" className="btn btn-primary btn-sm">
-                          Post
-                      </button>
-                  </form>
+              ></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary btn-sm">
+              Post
+            </button>
+          </form>
 
-                  {/*<p className="card-text">*/}
-                  {/*  Some quick example text to build on the card title and make up the*/}
-                  {/*  bulk of the card's content.*/}
-                  {/*</p>*/}
-              </div>
-          </div>
-      );
-  }
-  else {
-      return null;
+          {/*<p className="card-text">*/}
+          {/*  Some quick example text to build on the card title and make up the*/}
+          {/*  bulk of the card's content.*/}
+          {/*</p>*/}
+        </div>
+      </div>
+    );
+  } else {
+    return null;
   }
 };
