db9278086f7a4a398b70208cae6b211a56afa319
jwest115 <justinewest14@gmail.com>
2020-03-27 11:21:08 -0700

    removed home link from header

--- GlobalTraqs/frontend/src/components/layout/HeaderHook.js
+++ GlobalTraqs/frontend/src/components/layout/HeaderHook.js
@@ -158,11 +158,6 @@ function Header() {
 
       <div className="collapse navbar-collapse" id="navbarColor01">
         <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
-          </li>
           <li className="nav-item">
             <Link to="/faq" className="nav-link">
               Faq{" "}
