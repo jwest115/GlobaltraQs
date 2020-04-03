4d1d294dd35451a5885d262900f1d5089fe4b6a1
redxzeta <xzetanzero@gmail.com>
2020-04-03 12:02:00 -0700

    fixing issues

--- GlobalTraqs/GlobalTraqs/settings.py
+++ GlobalTraqs/GlobalTraqs/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'management',
     'django_cleanup.apps.CleanupConfig',
+    'django_extensions',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
