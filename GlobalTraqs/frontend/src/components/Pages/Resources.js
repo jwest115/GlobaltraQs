eacbf37379a6ff1e8d2f98c0fe5ae1a217bfd580
jwest115 <justinewest14@gmail.com>
2020-04-18 23:35:45 -0700

    fixed resource style

--- GlobalTraqs/frontend/src/components/Pages/Resources.js
+++ GlobalTraqs/frontend/src/components/Pages/Resources.js
@@ -61,7 +61,7 @@ export default function Resources() {
               </Col>
             </Row>
         </Col>
-        <Col md={5} className="offset-md-1c" style={{ paddingTop: "5%", paddingLeft: "30px" }}>
+        <Col md={5} className="offset-md-1" style={{ paddingTop: "5%", paddingLeft: "30px" }}>
             <div className="resources-sub-content-div">
             <h3 className="resources-title">what are we missing?</h3>
                 <p className="resources-sub-text">Drop us a line on our <span className="text-underline">Contact page</span> if you'd like to add to our ever-growing list of LGBTQ+ resources. Or just <span class="text-underline">send</span> us an email directly. </p>
