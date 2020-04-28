f9c1d9f1ea6917e4699da77498cb12e095ba8233
redxzeta <xzetanzero@gmail.com>
2020-04-28 14:12:37 -0700

    changing settings

--- GlobalTraqs/GlobalTraqs/settings.py
+++ GlobalTraqs/GlobalTraqs/settings.py
@@ -12,19 +12,20 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '946c#sv0!y1b-go8w)l@qd4j^74i22u4)i=5trrmj05mn40csy'
+# SECRET_KEY = '946c#sv0!y1b-go8w)l@qd4j^74i22u4)i=5trrmj05mn40csy'
 # SECRET_KEY = os.environ.get(
 #     'DJANGO_SECRET_KEY', '946c#sv0!y1b-go8w)l@qd4j^74i22u4)i=5trrmj05mn40csy')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = config('DEBUG', cast=bool)
 #DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', '.netlify.com', 'localhost']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',
+                 'https://globaltraqs.netlify.app/', 'localhost']
 
 
 # Application definition
@@ -123,18 +124,18 @@ DATABASES = {
     #     'PORT': '5432',
     # }
     #
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db1',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': config('NAME'),
-    #     'USER': config('USER'),
-    #     'PASSWORD': config('PASSWORD'),
-    #     'HOST': config('HOST'),
-    #     'PORT': config('PORT'),
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': 'db1',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
+    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     #     'NAME': 'arqivedb',
