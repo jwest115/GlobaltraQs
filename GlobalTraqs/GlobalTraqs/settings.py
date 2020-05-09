58323d3b9f2ba76463e05ffad7711bd451d5adc9
redxzeta <xzetanzero@gmail.com>
2020-05-08 20:58:57 -0700

    obscure email

--- GlobalTraqs/GlobalTraqs/settings.py
+++ GlobalTraqs/GlobalTraqs/settings.py
@@ -191,15 +191,10 @@ LOGOUT_ON_PASSWORD_CHANGE = False
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'resetglobaltraqs@gmail.com'
-EMAIL_HOST_PASSWORD = 'nmjpfuuvopvbmeri'
-
-# EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
-# EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default=''
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
